03c0d9b0c3cf4fdf2409ad7fbe24829e35efbde6
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-05-04 21:18:23 -0400

    first commit

---
+++ Week0/hello.py
@@ -0,0 +1,11 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Removes whitespace from string and capitalize
+# name = name.strip().title()
+
+# Capitalize user's name
+# name = name.title()
+
+print(f"Hello, {name}")
+
