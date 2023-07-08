def02c6e3eb0f3d05e4754502d60b543e986dd2a
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-07-08 18:23:25 -0400

    project refactor

--- project/project.py
+++ project/project.py
@@ -1,157 +1,180 @@
 import random
 
-password_pool = []
-letters = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
-
-count = 0
-
-uppercase = [x.upper() for x in letters]
-
-numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
-special_characters = [
-    "'",
-    '"',
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    ":",
-    ";",
-    "<",
-    ">",
-    "?",
-    "@",
-    "[",
-    "]",
-    "^",
-    "_",
-    "`",
-    "{",
-    "}",
-    "|",
-    "~",
-    "=",
-    "\\",
-]
-
 
 def main():
-    length = int(input("How many characters do you want in your password? "))
-    length_of_password = get_length(length)
+    password_pool = []
+    count = 0
+    length = get_length()
     while count == 0:
-        alpha()
-        upper()
-        number()
-        special()
+        choice = alpha()
+        if choice:
+            password_pool.extend(choice)
+            count += 1
+        choice2 = upper()
+        if choice2:
+            count += 1
+            password_pool.extend(choice2)
+        choice3 = number()
+        if choice3:
+            count += 1
+            password_pool.extend(choice3)
+        choice4 = special()
+        if choice4:
+            count += 1
+            password_pool.extend(choice4)
         if count != 0:
-            y = "".join(
-                [random.choice(password_pool) for _ in range(length_of_password)]
-            )
+            y = "".join([random.choice(password_pool) for _ in range(length)])
             print("Password:", y)
         else:
             print("Please select at least one condition")
 
 
-def get_length(length):
+def get_length():
     while True:
         try:
+            length = int(input("How many characters do you want in your password? "))
             if length < 8 or length > 128:
                 print("Number must be between 8 and 128")
             else:
                 return length
-        except ValueError:
+        except (ValueError, TypeError):
             print("Please choose a number between 8 and 128")
 
 
 def alpha():
     while True:
-        global count
+        letters = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ]
         choice = input("Do you want lowercase letters in your password? ").casefold()
         if choice == "yes" or choice == "y":
-            password_pool.extend(letters)
-            count += 1
-            return password_pool, count
+            return letters
         elif choice == "no" or choice == "n":
-            return password_pool
+            return
         else:
             print("Please answer yes or no")
 
 
 def upper():
     while True:
-        global count
+        letters = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ]
+        uppercase = [x.upper() for x in letters]
+
         choice2 = input("Do you want uppercase letter in your password? ").casefold()
+
         if choice2 == "yes" or choice2 == "y":
-            password_pool.extend(uppercase)
-            count += 1
-            return password_pool, count
+            return uppercase
         elif choice2 == "no" or choice2 == "n":
-            return password_pool
+            return
         else:
             print("Please answer yes or no")
 
 
 def number():
     while True:
-        global count
+        numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
         choice3 = input("Do you want numbers in your password? ").casefold()
         if choice3 == "yes" or choice3 == "y":
-            password_pool.extend(numbers)
-            count += 1
-            return password_pool, count
+            return numbers
         elif choice3 == "no" or choice3 == "n":
-            return password_pool
+            return
         else:
             print("Please answer yes or no")
 
 
 def special():
+    special_characters = [
+        "'",
+        '"',
+        "!",
+        "#",
+        "$",
+        "%",
+        "&",
+        "(",
+        ")",
+        "*",
+        "+",
+        ",",
+        "-",
+        ".",
+        "/",
+        ":",
+        ";",
+        "<",
+        ">",
+        "?",
+        "@",
+        "[",
+        "]",
+        "^",
+        "_",
+        "`",
+        "{",
+        "}",
+        "|",
+        "~",
+        "=",
+        "\\",
+    ]
     while True:
-        global count
         choice4 = input("Do you want special characters in your password? ").casefold()
         if choice4 == "yes" or choice4 == "y":
-            password_pool.extend(special_characters)
-            count += 1
-            return password_pool, count
+            return special_characters
         elif choice4 == "no" or choice4 == "n":
-            return password_pool
+            return
         else:
             print("Please answer yes or no")
 
--- project/test_project.py
+++ project/test_project.py
@@ -1,5 +1,1 @@
 from project import get_length
-
-
-def test_default():
-    assert get_length(9) == 9
